af854b29a6820f1dfab8d53e7c6a79d588641037
Sohail Orakzai <sohailokzz@gmail.com>
2023-04-01 00:30:14 +0500

    Working on homepage

--- lib/views/home_page.dart
+++ lib/views/home_page.dart
@@ -10,6 +10,8 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
+    double myHeight = MediaQuery.of(context).size.height;
+    double myWidth = MediaQuery.of(context).size.width;
     return SafeArea(
       child: Scaffold(
         body: Container(
@@ -22,6 +24,129 @@ class _HomePageState extends State<HomePage> {
                 Color(0xffFBC700),
               ],
             ),
+          ),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Padding(
+                padding: EdgeInsets.symmetric(
+                  vertical: myHeight * 0.03,
+                ),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  children: [
+                    Container(
+                      padding: EdgeInsets.symmetric(
+                        horizontal: myWidth * 0.02,
+                        vertical: myHeight * 0.005,
+                      ),
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(5),
+                        color: Colors.white.withOpacity(0.5),
+                      ),
+                      child: const Text(
+                        'Main Portfolio',
+                        style: TextStyle(
+                          fontSize: 16,
+                        ),
+                      ),
+                    ),
+                    const Text(
+                      'Top 10 Coins',
+                      style: TextStyle(
+                        fontSize: 16,
+                      ),
+                    ),
+                    const Text(
+                      'Experimental',
+                      style: TextStyle(
+                        fontSize: 16,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              Padding(
+                padding: EdgeInsets.symmetric(
+                  horizontal: myWidth * 0.04,
+                ),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    const Text(
+                      '\$ 7,742.20',
+                      style: TextStyle(
+                        fontSize: 24,
+                      ),
+                    ),
+                    Container(
+                      padding: EdgeInsets.all(myWidth * 0.015),
+                      height: myHeight * 0.05,
+                      width: myWidth * 0.1,
+                      decoration: BoxDecoration(
+                        color: Colors.white.withOpacity(0.5),
+                        shape: BoxShape.circle,
+                      ),
+                      child: Image.asset(
+                        'assets/icons/5.1.png',
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              Padding(
+                padding: EdgeInsets.symmetric(
+                  horizontal: myWidth * 0.04,
+                ),
+                child: Row(
+                  children: const [
+                    Text(
+                      '+ 162% all time',
+                      style: TextStyle(
+                        fontSize: 16,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              Container(
+                height: myHeight * 0.65,
+                width: myWidth,
+                decoration: const BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.only(
+                    topLeft: Radius.circular(50),
+                    topRight: Radius.circular(50),
+                  ),
+                ),
+                child: Padding(
+                  padding: EdgeInsets.symmetric(
+                    horizontal: myWidth * 0.08,
+                  ),
+                  child: Column(
+                    children: [
+                      SizedBox(
+                        height: myHeight * 0.03,
+                      ),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: const [
+                          Text(
+                            'Assets',
+                            style: TextStyle(
+                              fontSize: 20,
+                            ),
+                          ),
+                          Icon(
+                            Icons.add,
+                          )
+                        ],
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ],
           ),
         ),
       ),
